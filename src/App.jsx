391bb7e73e83037d8b7292c3fa86f7bb947b2a16
Darren Toner <darren.toner@outlook.com>
2024-09-24 14:19:40 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-white bg-opacity-20 backdrop-blur-lg p-10 rounded-xl shadow-xl text-center  ">
+        <h1 className="text-3xl font-bold text-white mb-2">Ciaran Maguire</h1>
+        <p className="text-xl text-white">Professional Mad Bastard</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
